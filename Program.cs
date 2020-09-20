69db0f19554ccd9dafd34ac0c5d6a9f22d3a902e
Isaac Pugliese <isaacpugliese2010@gmail.com>
2020-09-20 16:26:04 -0300

    Leet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                                     
+          string Frase;    
+          Console.WriteLine("Qual a frase a ser traduzida?");
+          Frase = Console.ReadLine()         
+         .ToUpper()
+         .Replace("A", "4")
+         .Replace("T", "7")
+         .Replace("O", "0")
+         .Replace("S", "5")
+         .Replace("I", "1")
+         .Replace("L", "1");              
+         Console.WriteLine("Sua frase em LEET fica:");
+         Console.WriteLine(Frase);              
+        }
+    }
+}
